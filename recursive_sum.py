98942c9d93e77ff23b79f487a91e191b336ee901
Juan Oliveira <robson.oficial13@gmail.com>
2023-09-13 17:18:20 -0300

    Soma de um array. Treinando recursão

---
+++ recursive_sum.py
@@ -0,0 +1,19 @@
+""" Implementação de um algoritmo de soma dos elementos de um array de forma recursiva."""
+def total_sum(array):
+    """soma de vetores por recursão. Usa a técnica divide and conquer para diminuir
+    o array em partes menores de si mesmo até chegar no caso base."""
+
+    # Caso base, array com um elemento.
+    if len(array) == 1:
+        return array[0]
+
+    # Caso o array venha vazio.
+    elif len(array) == 0:
+        return 0
+
+    # Soma o primeiro elemento com o resto do array
+    return array[0] + total_sum(array[1:])
+
+
+arr = [1,2,3,4,5]
+print(total_sum(arr))
