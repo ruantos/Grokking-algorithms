f656ed9c7a324a42f5c9652895fcc2eb5e2dbcaf
Juan Oliveira <robson.oficial13@gmail.com>
2023-09-13 17:35:37 -0300

    alterações

--- recursive_sum.py
+++ recursive_sum.py
@@ -3,14 +3,9 @@ def total_sum(array):
     """soma de vetores por recursão. Usa a técnica divide and conquer para diminuir
     o array em partes menores de si mesmo até chegar no caso base."""
 
-    # Caso base, array com um elemento.
-    if len(array) == 1:
-        return array[0]
-
-    # Caso o array venha vazio.
-    elif len(array) == 0:
+    # Caso base, array sem elemento.
+    if len(array) == 0:
         return 0
-
     # Soma o primeiro elemento com o resto do array
     return array[0] + total_sum(array[1:])
 
