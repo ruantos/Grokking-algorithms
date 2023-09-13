61f3e5f896889a0b17a384ed0d1bd065d961eb6b
Juan Oliveira <robson.oficial13@gmail.com>
2023-09-13 17:36:41 -0300

    alterações

--- recursive_counting.py
+++ recursive_counting.py
@@ -1,12 +1,10 @@
 """ Função recursiva para contar a quantidade de elementos em um array"""
 def counter(arr):
     """ Conta os elementos de um array recursivamente. """
-    if len(arr) == 1:
-        return 1
-    elif len(arr) == 0:
+    if len(arr) == 0:
         return 0
     return 1 + counter(arr[1:])
 
 
-array = []
+array = [1,2]
 print(counter(array))
